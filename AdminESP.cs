d951da613cf47cd3842003c7e41d12e731a9b7a6
mihaigsm2003 <51678688+mihaigsm2003@users.noreply.github.com>
2026-01-13 14:08:17 +0200

    update
    added "css_esp_p"  "Toggle Persistent Admin ESP"

--- AdminESP.cs
+++ AdminESP.cs
@@ -7,119 +7,205 @@ using CounterStrikeSharp.API.Modules.Cvars;
 using CounterStrikeSharp.API.Modules.Utils;
 
 namespace AdminESP;
+
 public sealed partial class AdminESP : BasePlugin, IPluginConfig<Config>
 {
     public override string ModuleName => "Admin ESP";
     public override string ModuleAuthor => "AquaVadis & GSM-RO";
-    public override string ModuleVersion => "1.2.2";
+    public override string ModuleVersion => "1.3.1"; 
     public override string ModuleDescription => "Plugin uses code borrowed from CS2Fixes / cs2kz-metamod / hl2sdk / unknown cheats and xstage from CS# discord";
-    public bool[] toggleAdminESP = new bool[64];
+
+    public bool[] toggleAdminESP = new bool[64]; 
+    public bool[] wantESP = new bool[64]; 
     public bool togglePlayersGlowing = false;
     public Config Config { get; set; } = new();
     private static readonly ConVar? _forceCamera = ConVar.Find("mp_forcecamera");
+
     public override void Load(bool hotReload)
     {
         RegisterListeners();
-        RegisterEventHandler<EventBotTakeover>(OnBotTakeoverFix); // Using EventBotTakeover with renamed method
+
         if (hotReload) {
-     
-            foreach (var player in Utilities.GetPlayers().Where(p => p is not null
+      
+            foreach (var player in Utilities.GetPlayers().Where(p => p is not null 
                                                                 && p.IsValid is true
                                                                 && p.Connected is PlayerConnectedState.PlayerConnected)) {
+
                 if (cachedPlayers.Contains(player) is not true)
                     cachedPlayers.Add(player);
+
             }
         }
+
     }
+
     public override void Unload(bool hotReload)
     {
+
         DeregisterListeners();
     }
-    [ConsoleCommand("css_esp", "Toggle Admin ESP")]
+
+    [ConsoleCommand("css_esp", "Toggle Admin ESP (non-persistent)")]
     [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
     public void OnToggleAdminEsp(CCSPlayerController? player, CommandInfo command)
     {
+
         if (player is null || player.IsValid is not true) return;
+
         if (AdminManager.PlayerHasPermissions(player, Config.AdminFlag) is not true) {
+
             SendMessageToSpecificChat(player, msg: "Admin ESP can only be used from {GREEN}admins{DEFAULT}!", print: PrintTo.Chat);
             return;  
         }
+        
         switch (player.PawnIsAlive) {
-           
+            
             case true:
+
                 if (Config.AllowDeadAdminESP is true) {
                     SendMessageToSpecificChat(player, msg: "You should be {RED}dead {DEFAULT}to use Admin ESP!", print: PrintTo.Chat);
                     return;
                 }
                 SendMessageToSpecificChat(player, msg: "Admin ESP is only allowed while {RED}spectating{DEFAULT}!", print: PrintTo.Chat);
+
             break;
             case false:
+
                 if (player.Team is CsTeam.Spectator) {
                     toggleAdminESP[player.Slot] = !toggleAdminESP[player.Slot];
+
                     if (toggleAdminESP[player.Slot] is true) {
-                       
+                        
                         if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
                             SetAllPlayersGlowing();
                         }
-                        //player.ReplicateConVar("mp_forcecamera", "0");
+                        
                     }
                     else {
+
                         if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
                             RemoveAllGlowingPlayers();
                         }
-                        //player.ReplicateConVar("mp_forcecamera", _forceCamera is not null ? _forceCamera.GetPrimitiveValue<int>().ToString() : "1");
+                        
                     }
-                   
-                    SendMessageToSpecificChat(player, msg: $"Admin ESP has been " + (toggleAdminESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
+                    
+
+                    SendMessageToSpecificChat(player, msg: $"Admin ESP has been " + (toggleAdminESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat); 
                     return;
                 }
                 else {
                     if (Config.AllowDeadAdminESP is true) {
                         toggleAdminESP[player.Slot] = !toggleAdminESP[player.Slot];
+
                         if (toggleAdminESP[player.Slot] is true) {
+
                             if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
                                 SetAllPlayersGlowing();
                             }
-                            //player.ReplicateConVar("mp_forcecamera", "0");
+                            
                         }
                         else {
+
                             if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
                                 RemoveAllGlowingPlayers();
                             }
-                            //player.ReplicateConVar("mp_forcecamera", _forceCamera is not null ? _forceCamera.GetPrimitiveValue<int>().ToString() : "1");
+                            
                         }
+
                         SendMessageToSpecificChat(player, msg: $"Admin ESP has been " + (toggleAdminESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
                         return;
-                   
+                    
                     }
                 }
+
                 SendMessageToSpecificChat(player, msg: "Admin ESP is only allowed in {RED}spectate mode{DEFAULT}!", print: PrintTo.Chat);
             break;
-       
+        
+
         }
     }
+
+    [ConsoleCommand("css_esp_p", "Toggle Persistent Admin ESP")]
+    [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
+    public void OnTogglePersistentAdminEsp(CCSPlayerController? player, CommandInfo command)
+    {
+
+        if (player is null || player.IsValid is not true) return;
+
+        if (AdminManager.PlayerHasPermissions(player, Config.AdminFlag) is not true) {
+
+            SendMessageToSpecificChat(player, msg: "Persistent Admin ESP can only be used from {GREEN}admins{DEFAULT}!", print: PrintTo.Chat);
+            return;  
+        }
+
+        wantESP[player.Slot] = !wantESP[player.Slot];
+        
+        switch (player.PawnIsAlive) {
+            
+            case true:
+
+                if (Config.AllowDeadAdminESP is true) {
+                    SendMessageToSpecificChat(player, msg: "You should be {RED}dead {DEFAULT}to use Persistent Admin ESP!", print: PrintTo.Chat);
+                    return;
+                }
+                SendMessageToSpecificChat(player, msg: "Persistent Admin ESP is only allowed while {RED}spectating{DEFAULT}!", print: PrintTo.Chat);
+
+            break;
+            case false:
+
+                if (player.Team is CsTeam.Spectator) {
+                    toggleAdminESP[player.Slot] = wantESP[player.Slot]; 
+
+                    if (toggleAdminESP[player.Slot] is true) {
+                        
+                        if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
+                            SetAllPlayersGlowing();
+                        }
+                    }
+                    else {
+
+                        if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
+                            RemoveAllGlowingPlayers();
+                        }
+                         }
+                    
+
+                    SendMessageToSpecificChat(player, msg: $"Persistent Admin ESP has been " + (wantESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat); 
+                    return;
+                }
+                else {
+                    if (Config.AllowDeadAdminESP is true) {
+                        toggleAdminESP[player.Slot] = wantESP[player.Slot]; 
+
+                        if (toggleAdminESP[player.Slot] is true) {
+
+                            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
+                                SetAllPlayersGlowing();
+                            }
+                        }
+                        else {
+
+                            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
+                                RemoveAllGlowingPlayers();
+                            }
+                        }
+
+                        SendMessageToSpecificChat(player, msg: $"Persistent Admin ESP has been " + (wantESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
+                        return;
+                    
+                    }
+                }
+
+                SendMessageToSpecificChat(player, msg: "Persistent Admin ESP is only allowed in {RED}spectate mode{DEFAULT}!", print: PrintTo.Chat);
+            break;
+        
+
+        }
+    }
+
     public void OnConfigParsed(Config config)
     {
         Config = config;
     }
 
-    // Handler for bot takeover event
-    private HookResult OnBotTakeoverFix(EventBotTakeover @event, GameEventInfo info)
-    {
-        var player = @event.Userid;
-        if (player == null || !player.IsValid) return HookResult.Continue;
-
-        if (toggleAdminESP[player.Slot])
-        {
-            toggleAdminESP[player.Slot] = false;
-            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true)
-            {
-                RemoveAllGlowingPlayers();
-            }
-            //player.ReplicateConVar("mp_forcecamera", _forceCamera is not null ? _forceCamera.GetPrimitiveValue<int>().ToString() : "1");
-            SendMessageToSpecificChat(player, msg: "Admin ESP has been {RED}disabled{DEFAULT} because you took over a bot!", print: PrintTo.Chat);
-        }
-
-        return HookResult.Continue;
-    }
 }
--- Core/Events.cs
+++ Core/Events.cs
@@ -2,251 +2,201 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Entities;
 using CounterStrikeSharp.API.Modules.Utils;
-
 namespace AdminESP;
-
 public partial class AdminESP
 {
-    
+   
     private void RegisterListeners()
     {
-
         RegisterListener<Listeners.OnClientAuthorized>(OnClientAuthorized);
         RegisterListener<Listeners.OnClientConnected>(OnClientConnected);
         RegisterListener<Listeners.OnClientPutInServer>(OnClientPutInServer);
         RegisterListener<Listeners.OnClientDisconnect>(OnClientDisconnected);
         RegisterListener<Listeners.CheckTransmit>(CheckTransmitListener);
-
         //register event listeners
         RegisterEventHandler<EventPlayerSpawn>(OnPlayerSpawn, HookMode.Pre);
         RegisterEventHandler<EventPlayerDeath>(OnPlayerDeath);
         RegisterEventHandler<EventRoundStart>(OnRoundStart);
-        
+       
     }
-
     private void DeregisterListeners()
     {
-
         RemoveListener<Listeners.OnClientAuthorized>(OnClientAuthorized);
         RemoveListener<Listeners.OnClientConnected>(OnClientConnected);
         RemoveListener<Listeners.OnClientPutInServer>(OnClientPutInServer);
         RemoveListener<Listeners.OnClientDisconnect>(OnClientDisconnected);
         RemoveListener<Listeners.CheckTransmit>(CheckTransmitListener);
-
         //deregister event listeners
         DeregisterEventHandler<EventPlayerSpawn>(OnPlayerSpawn, HookMode.Pre);
         DeregisterEventHandler<EventPlayerDeath>(OnPlayerDeath);
         DeregisterEventHandler<EventRoundStart>(OnRoundStart);
-
     }
-
     private void OnClientAuthorized(int slot, SteamID steamid)
     {
-
         var player = Utilities.GetPlayerFromSlot(slot);
         if(player == null || player.IsValid is not true) return;
-
         if (cachedPlayers.Contains(player) is not true)
             cachedPlayers.Add(player);
-        
+       
     }
-
     private void OnClientConnected(int slot)
     {
-
         var player = Utilities.GetPlayerFromSlot(slot);
         if(player == null || player.IsValid is not true) return;
-
         if (cachedPlayers.Contains(player) is not true)
             cachedPlayers.Add(player);
-        
+       
     }
-
     private void OnClientPutInServer(int slot)
     {
         var player = Utilities.GetPlayerFromSlot(slot);
         if (player is null || player.IsBot is not true) return;
-
         if (cachedPlayers.Contains(player) is not true)
             cachedPlayers.Add(player);
-        
+       
     }
-
-    // Core/Events.cs - MODIFICAT pentru a rezolva problema ESP la bot takeover
-// Adaugă logica de dezactivare instant în CheckTransmitListener (apelat frecvent)
-
-private void CheckTransmitListener(CCheckTransmitInfoList infoList)
-{
-
-    foreach ((CCheckTransmitInfo info, CCSPlayerController? player) in infoList)
+    // Core/Events.cs - MODIFICAT pentru persistent ESP la moarte/spectator si rezolvarea problema ESP la bot takeover
+    // Adaugă logica de dezactivare instant în CheckTransmitListener (apelat frecvent)
+    private void CheckTransmitListener(CCheckTransmitInfoList infoList)
     {
-
-        if (player is null || player.IsValid is not true) continue;
-
-        // NOU: Verifică și dezactivează ESP instant dacă player-ul este alive (inclusiv bot takeover)
-        if (player.PawnIsAlive && toggleAdminESP[player.Slot])
+        foreach ((CCheckTransmitInfo info, CCSPlayerController? player) in infoList)
         {
-            toggleAdminESP[player.Slot] = false;
-            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true)
+            if (player is null || player.IsValid is not true) continue;
+            // NOU: Verifică și dezactivează ESP instant dacă player-ul este alive (inclusiv bot takeover)
+            if (player.PawnIsAlive && toggleAdminESP[player.Slot])
             {
-                RemoveAllGlowingPlayers();
+                toggleAdminESP[player.Slot] = false;
+                if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true)
+                {
+                    RemoveAllGlowingPlayers();
+                }
+                SendMessageToSpecificChat(player, msg: "Admin ESP has been {RED}disabled{DEFAULT} because you are now alive (bot takeover)!", print: PrintTo.Chat);
             }
-            SendMessageToSpecificChat(player, msg: "Admin ESP has been {RED}disabled{DEFAULT} because you are now alive (bot takeover)!", print: PrintTo.Chat);
-        }
-
-        //itereate cached players
-        for (int i = 0; i < cachedPlayers.Count(); i++) {
-            
-            //leave self's observerPawn so it can spectate and check if feature is enabled
-            //we are clearing the whole spectator list as it doesn't work relaibly per person basis
-            if (Config.HideAdminSpectators is true) {
-
-                if (cachedPlayers[i] is null || cachedPlayers[i].IsValid is not true) continue;
-
-                //check if it 'us' in the current context and do the magic only if it's not
-                if (cachedPlayers[i].Slot != player.Slot) {
-
-                    //get the target's pawn
-                    var targetPawn = cachedPlayers[i].PlayerPawn.Value;
-                    if (targetPawn is null || targetPawn.IsValid is not true) continue;
-
-                    //get the target's observerpawn 
-                    var targetObserverPawn = cachedPlayers[i].ObserverPawn.Value;
-                    if (targetObserverPawn is null 
-                    || targetObserverPawn.IsValid is not true) continue;
-
-                    //we clear the spec list via clearing all of the observerTarget' pawns indexes 
-                    //from the Observer_services class that any cheat uses as a method to campare 
-                    //against current players in the server
-                    info.TransmitEntities.Remove((int)targetObserverPawn.Index);
+            //itereate cached players
+            for (int i = 0; i < cachedPlayers.Count(); i++) {
+               
+                //leave self's observerPawn so it can spectate and check if feature is enabled
+                //we are clearing the whole spectator list as it doesn't work relaibly per person basis
+                if (Config.HideAdminSpectators is true) {
+                    if (cachedPlayers[i] is null || cachedPlayers[i].IsValid is not true) continue;
+                    //check if it 'us' in the current context and do the magic only if it's not
+                    if (cachedPlayers[i].Slot != player.Slot) {
+                        //get the target's pawn
+                        var targetPawn = cachedPlayers[i].PlayerPawn.Value;
+                        if (targetPawn is null || targetPawn.IsValid is not true) continue;
+                        //get the target's observerpawn
+                        var targetObserverPawn = cachedPlayers[i].ObserverPawn.Value;
+                        if (targetObserverPawn is null
+                        || targetObserverPawn.IsValid is not true) continue;
+                        //we clear the spec list via clearing all of the observerTarget' pawns indexes
+                        //from the Observer_services class that any cheat uses as a method to campare
+                        //against current players in the server
+                        info.TransmitEntities.Remove((int)targetObserverPawn.Index);
+                    }
+                }
+                //check if admin has enabled ESP
+                if (toggleAdminESP[player.Slot] == true)
+                    continue;
+                   
+                //stop transmitting any entity from the glowingPlayers list
+                foreach (var glowingProp in glowingPlayers)
+                {
+                    if (glowingProp.Value.Item1 is not null && glowingProp.Value.Item1.IsValid is true
+                    && glowingProp.Value.Item2 is not null && glowingProp.Value.Item2.IsValid is true) {
+                        //prop one
+                        info.TransmitEntities.Remove((int)glowingProp.Value.Item1.Index);
+                        //prop two
+                        info.TransmitEntities.Remove((int)glowingProp.Value.Item2.Index);
+                    }
                 }
             }
-
-            //check if admin has enabled ESP 
-            if (toggleAdminESP[player.Slot] == true)
-                continue;
-                
-            //stop transmitting any entity from the glowingPlayers list
-            foreach (var glowingProp in glowingPlayers)
-            {
-
-                if (glowingProp.Value.Item1 is not null && glowingProp.Value.Item1.IsValid is true
-                && glowingProp.Value.Item2 is not null && glowingProp.Value.Item2.IsValid is true) {
-
-                    //prop one
-                    info.TransmitEntities.Remove((int)glowingProp.Value.Item1.Index);
-                    //prop two
-                    info.TransmitEntities.Remove((int)glowingProp.Value.Item2.Index);
-
-                }
-            }
-
         }
+       
     }
-
-   
-}
-
     public HookResult OnPlayerSpawn(EventPlayerSpawn @event, GameEventInfo info)
     {
         var player = @event.Userid;
-
-        if (player is null 
-        || player.IsValid is not true 
+        if (player is null
+        || player.IsValid is not true
         || player.Connected is not PlayerConnectedState.PlayerConnected) return HookResult.Continue;
-
-        AddTimer(2f, () => {
-
-            if (player is null 
-            || player.IsValid is not true 
-            || player.Connected is not PlayerConnectedState.PlayerConnected) return;
-
-            //force hiding the glowing props no matter what upon spawn(this is mostly ignored on the first spawn of the round)
-            toggleAdminESP[player.Slot] = false;
-        });
-
+        // Dezactivează imediat (fără delay)
+        toggleAdminESP[player.Slot] = false;
+        if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true)
+        {
+            RemoveAllGlowingPlayers();
+        }
+        SendMessageToSpecificChat(player, msg: "Admin ESP has been {RED}disabled{DEFAULT} because you spawned!", print: PrintTo.Chat);
         return HookResult.Continue;
     }
-
     public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
     {
-
         for (int i = 0; i < cachedPlayers.Count(); i++) {
-
             if (cachedPlayers[i] is null || cachedPlayers[i].IsValid is not true) continue;
-
-            if (toggleAdminESP[cachedPlayers[i].Slot] is true && cachedPlayers[i].Team is CsTeam.Spectator && Config.SkipSpectatingEsps is true) 
+            if (toggleAdminESP[cachedPlayers[i].Slot] is true && cachedPlayers[i].Team is CsTeam.Spectator && Config.SkipSpectatingEsps is true)
                 continue;
-            
+           
             toggleAdminESP[cachedPlayers[i].Slot] = false;
-
         }
-
         if (togglePlayersGlowing is true)
             togglePlayersGlowing = false;
-
         //check if there are espering admins and if SkipSpectatingEsps is true, to restore the glowing props
         Server.NextFrame(() => {
-
             //remove props if there isn't any espering admin/s
             if (AreThereEsperingAdmins() is false) {
-
                 RemoveAllGlowingPlayers();
                 return;
             }
-
             //respawn the glowing props if there are espering admins and SkipSpectatingEsps is true
             if (AreThereEsperingAdmins() is true && Config.SkipSpectatingEsps is true)
                 SetAllPlayersGlowing();
-
         });
-
         return HookResult.Continue;
     }
-
     public HookResult OnPlayerDeath(EventPlayerDeath @event, GameEventInfo info)
     {
         var player = @event.Userid;
-
-        if (player is null 
-        || player.IsValid is not true 
+        if (player is null
+        || player.IsValid is not true
         || player.Connected is not PlayerConnectedState.PlayerConnected) return HookResult.Continue;
-
         //remove glowing prop if player has one upon death
         if (glowingPlayers.ContainsKey(player.Slot) is true) {
-
             if (glowingPlayers[player.Slot].Item1 is not null && glowingPlayers[player.Slot].Item1.IsValid is true
             && glowingPlayers[player.Slot].Item2 is not null && glowingPlayers[player.Slot].Item2.IsValid is true) {
-                
+               
                 //remove previous modelRelay prop
                 glowingPlayers[player.Slot].Item1.AcceptInput("Kill");
                 //remove previous modelGlow prop
                 glowingPlayers[player.Slot].Item2.AcceptInput("Kill");
-
             }
-
             //remove player from the list
             glowingPlayers.Remove(player.Slot);
         }
-
+        // NOU: Activează ESP automat dacă persistent este on și condițiile sunt îndeplinite - cu delay mai mare pentru stabilizare
+        AddTimer(0.5f, () => {
+            if (player is null || player.IsValid is not true || player.PawnIsAlive) return; // Verifică din nou dacă e mort
+            if (wantESP[player.Slot] && (player.Team == CsTeam.Spectator || Config.AllowDeadAdminESP))
+            {
+                toggleAdminESP[player.Slot] = true;
+                togglePlayersGlowing = true; // Asigură că glowing-ul global e activat
+                SetAllPlayersGlowing(); // Recrează glowing props
+                //player.ReplicateConVar("mp_forcecamera", "0"); // Dacă e necesar
+                SendMessageToSpecificChat(player, msg: "Admin ESP has been {GREEN}auto-enabled{DEFAULT} after death!", print: PrintTo.Chat);
+            }
+        });
         return HookResult.Continue;
     }
-
     private void OnClientDisconnected(int slot)
     {
-
         var player = Utilities.GetPlayerFromSlot(slot);
         if (player == null || player.IsValid is not true) return;
-
         //set 'toggleAdminESP' to false regardless, on player disconnected
         //thus avoid any lingering glowing props
         toggleAdminESP[slot] = false;
-
+        // NOU: Resetează și starea persistentă la disconnect (opțional; poți elimina dacă vrei să persiste peste reconectări)
+        wantESP[slot] = false;
         //remove player from cached list
         if (cachedPlayers.Contains(player) is true)
             cachedPlayers.Remove(player);
-        
+       
     }
-
-
 }
